5ffaf5a6d54001f58d1fd1ff48d41e7a7257d82b
rakin406 <rakinhacker@gmail.com>
2024-06-12 23:02:35 +0600

    fix: get proper random state instead of empty

--- grid_world_ai/ai.py
+++ grid_world_ai/ai.py
@@ -1,3 +1,4 @@
+import random
 from constants import *
 import pyray as pr
 
@@ -11,11 +12,11 @@ class AI:
         self.state = self.random_state()    # Initial state of the agent
 
     def random_state(self):
-        random_state = ()
-        while random_state == self.goal_state:
-            random_state = (random.randrange(0, WINDOW_WIDTH, self.grid_spacing),
-                            random.randrange(0, WINDOW_HEIGHT, self.grid_spacing))
-        return random_state
+        while True:
+            state = (random.randrange(0, WINDOW_WIDTH, self.grid_spacing),
+                     random.randrange(0, WINDOW_HEIGHT, self.grid_spacing))
+            if state != self.goal_state:
+                return state
 
     def reset(self):
         """Resets agent state to a random coordinate."""
@@ -24,4 +25,4 @@ class AI:
 
     def draw(self):
         pr.draw_rectangle(self.state[0], self.state[1], self.grid_spacing,
-                          self.grid_spacing, pr.RED)
+                          self.grid_spacing, pr.BLUE)
