cb9304317355880b7bc626267a9579af25ebb93a
rakin406 <rakinhacker@gmail.com>
2024-06-12 17:57:50 +0600

    feat: create and display window

--- grid_world_ai/render_system.py
+++ grid_world_ai/render_system.py
@@ -1,8 +1,19 @@
 """System for rendering entities in the world."""
 
-from pecs.registry import Registry
+from constants import *
+import pecs
+from pyray import *
 
 
 class RenderSystem:
-    def __init__(self, registry: Registry):
-        pass
+    def __init__(self, registry: pecs.Registry):
+        self.registry = registry
+
+    def display(self):
+        init_window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+        while not window_should_close():
+            begin_drawing()
+            clear_background(WHITE)
+            draw_text("Hello world", 190, 200, 20, VIOLET)
+            end_drawing()
+        close_window()
